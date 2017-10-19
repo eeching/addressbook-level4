db0cd80dc2dc9eb2fdda81e5e426ba2d42f55e17
xuyiqing <xuyiqing613@gmail.com>
2017-10-19 11:40:58 +0800

    style check

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -43,7 +43,7 @@ public class LogicManager extends ComponentManager implements Logic {
 
             try {
                 result = command.execute();
-            } catch(IllegalValueException e) {
+            } catch (IllegalValueException e) {
                 throw new AssertionError("wrong input value");
             }
 
